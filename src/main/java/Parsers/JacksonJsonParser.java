36c5d1015273830b9cb930cf5f07276f976218f7
cc484 <cc44@protonmail.com>
2023-07-26 23:51:31 -0400

    Updated JacksonJsonParser

--- src/main/java/Parsers/JacksonJsonParser.java
+++ src/main/java/Parsers/JacksonJsonParser.java
@@ -1,7 +1,30 @@
 package Parsers;
 
+import Models.Flight;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sun.tools.javac.Main;
+
+import java.io.File;
+import java.io.IOException;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class JacksonJsonParser {
+    private static final Logger loggerJackson = LogManager.getLogger(Main.class);
+    public static void main(String[] args) {
 
+        ObjectMapper objectMapper = new ObjectMapper();
 
+        //configure object mapper to deserialize array
+        objectMapper.configure(DeserializationFeature.USE_JAVA_ARRAY_FOR_JSON_ARRAY, true);
+
+        try {
+            Flight[] flights = objectMapper.readValue(new File("src/main/resources/JSON/flights.json"), Flight[].class);
+
+        } catch(IOException ie) {
+            loggerJackson.error("There was an IO exception with the Object Mapper");
+        }
+    }
 
 }
